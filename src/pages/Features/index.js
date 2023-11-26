fa5904bb66afa702a21a1c5ba84887776c18152f
kellyzhang <golive.future@gmail.com>
2023-11-26 07:55:29 -0500

    features page

--- src/pages/Features/index.js
+++ src/pages/Features/index.js
@@ -1,31 +1,32 @@
 import React from "react";
 
-
 import PageLayout from "../../layouts/PageLayout";
 
-
 //Images
-import Shape1 from '../../assets/images/home-banner/shape1.png';
+import Shape1 from "../../assets/images/home-banner/shape1.png";
 import Shape3 from "./../../assets/images/home-banner/shape3.png";
 
-import decentralized1 from "./../../assets/images/features/decentralized1.jpg";
-import decentralized2 from "./../../assets/images/features/decentralized2.jpg";
-import accurate1 from "./../../assets/images/features/accurate1.jpg";
-import accurate2 from "./../../assets/images/features/accurate2.jpg";
 
+import infrastructure1 from "./../../assets/images/features/infrastructure1.png";
+import infrastructure2 from "./../../assets/images/features/infrastructure2.png";
+import integration1 from "./../../assets/images/features/integration1.png";
+import integration2 from "./../../assets/images/features/integration2.png";
+import prediction1 from "./../../assets/images/features/prediction1.png";
+import prediction2 from "./../../assets/images/features/prediction2.png";
+import accessibility1 from "./../../assets/images/features/accessibility1.png";
+import accessibility2 from "./../../assets/images/features/accessibility2.png";
 
 const ImageBox = ({ image, changeClass }) => {
   return (
     <div className="col-6">
       <div className={`image-box ${changeClass}`}>
-        <img src={image} alt="" style={{visibility : 'visible!important'}}/>
+        <img src={image} alt="" style={{ visibility: "visible!important" }} />
       </div>
     </div>
   );
 };
 
 function Features() {
-  
   return (
     <>
       <div className="page-content">
@@ -33,22 +34,36 @@ function Features() {
         <section className="content-inner about-sec bg-primary-light">
           <div className="container">
             <div className="row about-bx2 style-1 align-items-center">
-              
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+                    <ImageBox
+                      image={infrastructure1}
+                      changeClass="image-box-1"
+                    />
+                    <ImageBox
+                      image={infrastructure2}
+                      changeClass="image-box-2"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">Infrastructure</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    At UP1, we are redefining the foundation of blockchain
+                    technology. Our state-of-the-art infrastructure is built to
+                    provide unparalleled efficiency, security, and scalability.
+                    By leveraging advanced consensus mechanisms and a robust
+                    architectural framework, UP1 ensures fast, secure, and
+                    seamless transactions. Our infrastructure is designed to
+                    support high transaction throughput while maintaining low
+                    latency, making it ideal for a wide range of applications,
+                    from financial services to supply chain management. With
+                    UP1, we are setting new standards for blockchain
+                    infrastructure, ensuring that our platform is not only
+                    powerful but also eco-friendly and sustainable.
                   </p>
                 </div>
               </div>
@@ -57,61 +72,58 @@ function Features() {
                 <div className="section-head">
                   <h2 className="title">Prediction Markets</h2>
                   <p className="m-0 lh-base">
-                    Detail how UP1 provides accurate and timely insights into
-                    various market trends and events. Emphasize the platform's
-                    commitment to data accuracy and real-time updates.
+                  UP1 is at the forefront of transforming prediction markets through blockchain technology. Our platform provides a transparent, decentralized, and tamper-proof environment for users to speculate on the outcome of events ranging from financial markets to sports and elections. By harnessing the power of crowd wisdom and blockchain, UP1's prediction markets offer a unique opportunity for market forecasting and risk assessment. Participants benefit from a global pool of knowledge and insights, ensuring fair and accurate market predictions. UP1 is dedicated to bringing a new level of transparency and efficiency to prediction markets, making them more accessible and reliable than ever before.
                   </p>
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70">
-                    <ImageBox image={accurate1} changeClass="image-box-1" />
-                    <ImageBox image={accurate2} changeClass="image-box-2" />
+                    <ImageBox image={prediction1} changeClass="image-box-1" />
+                    <ImageBox image={prediction2} changeClass="image-box-2" />
                   </div>
                 </div>
               </div>
-              
+
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+                    <ImageBox
+                      image={integration1}
+                      changeClass="image-box-1"
+                    />
+                    <ImageBox
+                      image={integration2}
+                      changeClass="image-box-2"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">Integration</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                  Integration is at the heart of UP1's philosophy. We understand the importance of interoperability in today’s fragmented blockchain landscape. That's why UP1 is designed to seamlessly integrate with a multitude of systems, platforms, and applications. Our comprehensive APIs and developer tools make it easy for businesses and developers to adopt and build on the UP1 protocol. Whether it's integrating with existing financial systems, supply chains, or emerging technologies like IoT and AI, UP1's flexible integration capabilities enable a more connected and efficient blockchain ecosystem. With UP1, the potential for innovation is limitless.
                   </p>
                 </div>
               </div>
 
-
-
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
-                  <h2 className="title">Accesibility </h2>
+                  <h2 className="title">Accessibility </h2>
                   <p className="m-0 lh-base">
-                    Detail how UP1 provides accurate and timely insights into
-                    various market trends and events. Emphasize the platform's
-                    commitment to data accuracy and real-time updates.
+                  Accessibility is a cornerstone of UP1. We are committed to making blockchain technology accessible to everyone, regardless of their technical background or geographic location. Our user-friendly interface, educational resources, and community support are designed to lower the entry barrier to blockchain technology. UP1’s multilingual support and inclusive design ensure that our platform caters to a global audience. We also focus on mobile accessibility, recognizing the importance of smartphones in today's digital age. At UP1, we believe in empowering users by providing them with the tools and knowledge to explore the full potential of blockchain technology.
                   </p>
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70">
-                    <ImageBox image={accurate1} changeClass="image-box-1" />
-                    <ImageBox image={accurate2} changeClass="image-box-2" />
+                    <ImageBox image={accessibility1} changeClass="image-box-1" />
+                    <ImageBox image={accessibility2} changeClass="image-box-2" />
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
           <img className="bg-shape1" src={Shape1} alt="" />
@@ -119,8 +131,6 @@ function Features() {
           <img className="bg-shape3" src={Shape3} alt="" />
           <img className="bg-shape4" src={Shape3} alt="" />
         </section>
-        
-        
       </div>
     </>
   );
--- src/pages/Protocol/index.js
+++ src/pages/Protocol/index.js
@@ -3,45 +3,53 @@ import React from "react";
 import PageLayout from "../../layouts/PageLayout";
 
 //Images
-import Shape1 from '../../assets/images/home-banner/shape1.png';
+import Shape1 from "../../assets/images/home-banner/shape1.png";
 import Shape3 from "./../../assets/images/home-banner/shape3.png";
 
-import decentralized1 from "./../../assets/images/features/decentralized1.jpg";
+import Logo from "./../../assets/images/protocol/UP1Logo2.png";
 import decentralized2 from "./../../assets/images/features/decentralized2.jpg";
+import decentralized1 from "./../../assets/images/features/decentralized1.jpg";
 
 const ImageBox = ({ image, changeClass }) => {
   return (
     <div className="col-6">
       <div className={`image-box ${changeClass}`}>
-        <img src={image} alt=""/>
+        <img src={image} alt="" />
       </div>
     </div>
   );
 };
 
-function Protocol() {  
-  
+function Protocol() {
   return (
     <>
       <div className="page-content">
-        <PageLayout pageTitle="Protocol" desc="This protocol is coming best now."/>
+        <PageLayout
+          pageTitle="Protocol"
+          desc="This protocol is coming best now."
+        />
         <section className="content-inner about-sec bg-primary-light">
           <div className="container">
             <div className="about-bx2 style-1 align-items-center">
-            <div className="">
-                <div className="dz-media">
-                  <div className="justify-content-center m-b70 d-flex">
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+              <div className="">
+                <div className="dz-media d-flex justify-content-center">
+                  <div
+                    className="justify-content-center m-b70 d-flex"
+                    style={{ width: "70%" }}
+                  >
+                    <ImageBox image={Logo} changeClass="image-box-2" />
+                  </div>
                 </div>
               </div>
               <div className="about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title text-center">Whitepaper</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    Discusses our commitment to robust security measures and
+                    decentralized governance, ensuring transparency and
+                    trustworthiness. It serves as a roadmap, guiding readers
+                    through our future plans, milestones, and the overarching
+                    vision of UP1 in revolutionizing the blockchain space.
                   </p>
                 </div>
               </div>
@@ -55,22 +63,34 @@ function Protocol() {
         <section className="content-inner about-sec bg-light">
           <div className="container">
             <div className="row about-bx2 style-1 align-items-center">
-              
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+                    <ImageBox
+                      image={decentralized1}
+                      changeClass="image-box-1"
+                    />
+                    <ImageBox
+                      image={decentralized2}
+                      changeClass="image-box-2"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">UP1 token</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    The UP1 Token is the native cryptocurrency of our platform,
+                    designed to facilitate transactions, reward network
+                    participation, and enable governance. As a utility token, it
+                    plays a crucial role in the operation of Automated Market
+                    Makers, staking, and payment of transaction fees. The UP1
+                    Token is built with an emphasis on stability and
+                    scalability, ensuring a seamless and efficient user
+                    experience. It also empowers token holders with governance
+                    rights, allowing them to vote on key decisions and
+                    contribute to the development of the UP1 ecosystem.
                   </p>
                 </div>
               </div>
@@ -81,29 +101,46 @@ function Protocol() {
         <section className="content-inner about-sec bg-primary-light">
           <div className="container">
             <div className="about-bx2 style-1 align-items-center">
-                <div className="about-content ps-lg-5 m-b100">
-                    <div className="section-head">
-                    <h2 className="title">Automated Market Makers</h2>
-                    <p className="m-0 lh-base">
-                        Explain how blockchain ensures transparency, security, and
-                        fairness in prediction outcomes. Highlight the advantages of
-                        decentralized prediction markets.
-                    </p>
-                    </div>
+              <div className="about-content ps-lg-5 m-b100">
+                <div className="section-head">
+                  <h2 className="title">Automated Market Makers</h2>
+                  <p className="m-0 lh-base">
+                    UP1 introduces a cutting-edge approach to trading with its
+                    Automated Market Makers. By leveraging smart contract
+                    technology, our AMMs provide a decentralized and transparent
+                    trading environment. This system eliminates the need for
+                    traditional order books, offering liquidity and price
+                    discovery directly on the blockchain. Our AMMs are designed
+                    to optimize efficiency and minimize slippage, providing
+                    users with a reliable and user-friendly trading experience.
+                  </p>
                 </div>
-                <div className="row">
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">hello</span>
-                    </div>
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">white</span>
-                    </div>
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">blue</span>
-                    </div>
+              </div>
+              <div className="row">
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={decentralized2}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  <span className="text-center">hello</span>
+                </div>
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={decentralized2}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  <span className="text-center">white</span>
+                </div>
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={decentralized2}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  <span className="text-center">blue</span>
+                </div>
               </div>
             </div>
           </div>
@@ -115,21 +152,32 @@ function Protocol() {
         <section className="content-inner about-sec bg-light">
           <div className="container">
             <div className="row about-bx2 style-1 align-items-center">
-              
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end justify-content-center m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                  </div>                                    
+                    <ImageBox
+                      image={decentralized1}
+                      changeClass="image-box-1"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">Security</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    Foundation of our entire blockchain ecosystem. We have
+                    engineered our platform with the highest security standards
+                    to safeguard user data and assets. Our multi-layered
+                    security architecture includes advanced encryption, rigorous
+                    smart contract auditing, and continuous network monitoring
+                    to prevent unauthorized access and detect potential threats.
+                    We are dedicated to maintaining a secure environment,
+                    incorporating both traditional cybersecurity measures and
+                    innovative blockchain-specific defenses. With UP1, users and
+                    developers can operate with the confidence that their
+                    transactions and applications are protected in a
+                    state-of-the-art secure blockchain network.
                   </p>
                 </div>
               </div>
@@ -144,17 +192,30 @@ function Protocol() {
                 <div className="section-head">
                   <h2 className="title">Decentralized Governance</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    Decentralized Governance is the cornerstone of UP1's
+                    commitment to a democratic and transparent ecosystem. We
+                    believe that the future of blockchain lies in empowering our
+                    community to have a direct impact on the protocol's
+                    development and direction. Our governance model is designed
+                    to be inclusive, allowing UP1 token holders to propose,
+                    vote, and implement changes to the network. This approach
+                    ensures that decisions are made in the best interest of the
+                    community, fostering an environment of collaboration and
+                    mutual respect. Decentralized Governance at UP1 is not just
+                    about giving a voice to the community; it's about building a
+                    blockchain protocol that truly reflects the collective
+                    wisdom and vision of its users.
                   </p>
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70 justify-content-center">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                  </div>                                    
+                    <ImageBox
+                      image={decentralized1}
+                      changeClass="image-box-1"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -164,29 +225,43 @@ function Protocol() {
         <section className="content-inner about-sec bg-light">
           <div className="container">
             <div className="about-bx2 style-1 align-items-center">
-                <div className="about-content ps-lg-5 m-b100">
-                    <div className="section-head">
-                    <h2 className="title text-center">Roadmap</h2>
-                    <p className="m-0 lh-base">
-                        Explain how blockchain ensures transparency, security, and
-                        fairness in prediction outcomes. Highlight the advantages of
-                        decentralized prediction markets.
-                    </p>
-                    </div>
+              <div className="about-content ps-lg-5 m-b100">
+                <div className="section-head">
+                  <h2 className="title text-center">Roadmap</h2>
+                  <p className="m-0 lh-base">
+                    Explain how blockchain ensures transparency, security, and
+                    fairness in prediction outcomes. Highlight the advantages of
+                    decentralized prediction markets.
+                  </p>
                 </div>
+              </div>
               <div className="">
                 <div className="dz-media">
                   <div className="row m-b70">
-                    <img src={decentralized1} className='col-4' changeClass="image-box-1" alt="decentral"/>
-                    <img src={decentralized2} className='col-4' changeClass="image-box-2" alt="decentral"/>
-                    <img src={decentralized1} className='col-4' changeClass="image-box-1" alt="decentral"/>
-                  </div>                                    
+                    <img
+                      src={decentralized1}
+                      className="col-4"
+                      changeClass="image-box-1"
+                      alt="decentral"
+                    />
+                    <img
+                      src={decentralized2}
+                      className="col-4"
+                      changeClass="image-box-2"
+                      alt="decentral"
+                    />
+                    <img
+                      src={decentralized1}
+                      className="col-4"
+                      changeClass="image-box-1"
+                      alt="decentral"
+                    />
+                  </div>
                 </div>
               </div>
-              
             </div>
           </div>
-        </section>         
+        </section>
       </div>
     </>
   );
