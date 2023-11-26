1445c7ae17104f6e98c3b2a9d2f7d87a1bc84412
kellyzhang <golive.future@gmail.com>
2023-11-26 16:18:54 -0500

    contents of incentives page

--- src/pages/Incentives/index.js
+++ src/pages/Incentives/index.js
@@ -3,45 +3,55 @@ import React from "react";
 import PageLayout from "../../layouts/PageLayout";
 
 //Images
-import Shape1 from '../../assets/images/home-banner/shape1.png';
+import Shape1 from "../../assets/images/home-banner/shape1.png";
 import Shape3 from "./../../assets/images/home-banner/shape3.png";
 
 import decentralized1 from "./../../assets/images/features/decentralized1.jpg";
 import decentralized2 from "./../../assets/images/features/decentralized2.jpg";
+import reward from "./../../assets/images/incentives/reward.png";
+import staking from "./../../assets/images/incentives/staking.png";
+import liquidity1 from "./../../assets/images/incentives/liquidity1.png";
+import liquidity2 from "./../../assets/images/incentives/liquidity2.png";
+import liquidity3 from "./../../assets/images/incentives/liquidity3.png";
+import prediction from "./../../assets/images/incentives/prediction.png";
+import governance from "./../../assets/images/incentives/governance.png";
 
 const ImageBox = ({ image, changeClass }) => {
   return (
     <div className="col-6">
       <div className={`image-box ${changeClass}`}>
-        <img src={image} alt=""/>
+        <img src={image} alt="" />
       </div>
     </div>
   );
 };
 
 function Incentives() {
-    
   return (
     <>
       <div className="page-content">
-        <PageLayout pageTitle="Incentives" desc="Staking and liquidity provision rewards"/>
+        <PageLayout
+          pageTitle="Incentives"
+          desc="Staking and liquidity provision rewards"
+        />
         <section className="content-inner about-sec bg-primary-light">
           <div className="container">
             <div className="about-bx2 style-1 align-items-center">
-            <div className="">
+              <div className="">
                 <div className="dz-media">
                   <div className="justify-content-center m-b70 d-flex">
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+                    <ImageBox image={reward} changeClass="image-box-2" />
+                  </div>
                 </div>
               </div>
               <div className="about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title text-center">Reward Systems</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    The UP1 token ecosystem is designed to reward its users
+                    proactively, creating a vibrant and participatory community.
+                    Our reward systems are multifaceted, catering to different
+                    aspects of user engagement.
                   </p>
                 </div>
               </div>
@@ -55,22 +65,21 @@ function Incentives() {
         <section className="content-inner about-sec bg-light">
           <div className="container">
             <div className="row about-bx2 style-1 align-items-center">
-              
               <div className="col-lg-6">
                 <div className="dz-media">
-                  <div className="row align-items-end m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                    <ImageBox image={decentralized2} changeClass="image-box-2" />
-                  </div>                                    
+                  <div className="row align-items-end m-b70 d-flex justify-content-center">
+                    <ImageBox image={staking} changeClass="image-box-2" />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">Staking Mechanism</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    Staking UP1 tokens is more than just an investment, it's an
+                    opportunity to be part of a growing community, contribute to
+                    network health, and have a say in the project's direction.
+                    Join us in building a robust and thriving UP1 ecosystem!
                   </p>
                 </div>
               </div>
@@ -81,29 +90,43 @@ function Incentives() {
         <section className="content-inner about-sec bg-primary-light">
           <div className="container">
             <div className="about-bx2 style-1 align-items-center">
-                <div className="about-content ps-lg-5 m-b100">
-                    <div className="section-head">
-                    <h2 className="title">Liquidity Provision Rewards</h2>
-                    <p className="m-0 lh-base">
-                        Explain how blockchain ensures transparency, security, and
-                        fairness in prediction outcomes. Highlight the advantages of
-                        decentralized prediction markets.
-                    </p>
-                    </div>
+              <div className="about-content ps-lg-5 m-b100">
+                <div className="section-head">
+                  <h2 className="title">Liquidity Provision Rewards</h2>
+                  <p className="m-0 lh-base">
+                    The Liquidity Provision Rewards program of the UP1 token
+                    aims to create a robust and sustainable financial ecosystem,
+                    where the contributions of each participant are valued and
+                    rewarded. This approach not only benefits individual
+                    providers but also strengthens the entire UP1 community.
+                  </p>
                 </div>
-                <div className="row">
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">hello</span>
-                    </div>
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">white</span>
-                    </div>
-                    <div className="d-flex col-4 flex-column">
-                        <img src={decentralized2} changeClass="image-box-1" alt="decentral"/>
-                        <span className="text-center">blue</span>
-                    </div>
+              </div>
+              <div className="row">
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={liquidity1}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  {/* <span className="text-center">hello</span> */}
+                </div>
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={liquidity2}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  {/* <span className="text-center">white</span> */}
+                </div>
+                <div className="d-flex col-4 flex-column">
+                  <img
+                    src={liquidity3}
+                    changeClass="image-box-1"
+                    alt="decentral"
+                  />
+                  {/* <span className="text-center">blue</span> */}
+                </div>
               </div>
             </div>
           </div>
@@ -115,21 +138,22 @@ function Incentives() {
         <section className="content-inner about-sec bg-light">
           <div className="container">
             <div className="row about-bx2 style-1 align-items-center">
-              
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end justify-content-center m-b70">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                  </div>                                    
+                    <ImageBox image={prediction} changeClass="image-box-1" />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b100">
                 <div className="section-head">
                   <h2 className="title">Prediction and Accuracy Bonuses</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    The Prediction and Accuracy Bonuses of the UP1 token are
+                    designed to incentivize and reward market analysis and
+                    foresight. This feature not only enhances user engagement
+                    but also contributes to a more informed and active community
+                    within the UP1 ecosystem.
                   </p>
                 </div>
               </div>
@@ -144,23 +168,25 @@ function Incentives() {
                 <div className="section-head">
                   <h2 className="title">Governance Participation</h2>
                   <p className="m-0 lh-base">
-                    Explain how blockchain ensures transparency, security, and
-                    fairness in prediction outcomes. Highlight the advantages of
-                    decentralized prediction markets.
+                    UP1 Token's governance model is designed to promote active
+                    and meaningful participation, ensuring that the UP1
+                    ecosystem evolves in a way that benefits all stakeholders.
+                    It's not just about holding a token; it's about being part
+                    of a community that values your input and vision.
                   </p>
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="dz-media">
                   <div className="row align-items-end m-b70 justify-content-center">
-                    <ImageBox image={decentralized1} changeClass="image-box-1" />
-                  </div>                                    
+                    <ImageBox image={governance} changeClass="image-box-1" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <img className="bg-shape4" src={Shape3} alt="" />
-        </section>   
+        </section>
       </div>
     </>
   );
